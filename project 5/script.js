acaf382eef9db2d2282eda9510071a8f40263581
Sohail Shaikh <sohailhacker7786@gmail.com>
2023-10-15 14:58:05 +0530

    modified:   project 5/style.css

--- project 5/script.js
+++ project 5/script.js
@@ -1,9 +1,34 @@
-var arr = [1,2,3,4,5]
+var arr = [
+    {dp:"https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+    ,story:"mypic.jpg"},
+    {dp:"https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+    ,story:"https://plus.unsplash.com/premium_photo-1673356301861-d555ce5972ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"},
+    {dp:"https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80"
+    ,story:"https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"},
+    {dp:"https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+    ,story:"https://plus.unsplash.com/premium_photo-1675186049366-64a655f8f537?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"},
+]
 
 var clutter = ""
 
-arr.forEach(function(elem){
-    clutter+=elem
+arr.forEach(function(elem,idx){
+    clutter += `<div class="story">
+    <img id="${idx}" src="${elem.dp}" alt="">
+</div>`
+    
 
 })  
+var storiyan =document.querySelector("#storyan")
+
+storiyan.innerHTML = clutter
+storiyan.addEventListener("click",function(dets){
+    document.querySelector("#full-scr").style.display = "block"
+    document.querySelector("#full-scr").style.backgroundImage = `url(${arr[dets.target.id].story})`
+    setTimeout(function(){
+        document.querySelector("#full-scr").style.display = "none"
+    },3000)
+
+
+});
+
 console.log(clutter)
